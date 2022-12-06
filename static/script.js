84883d2f5d810ac722f5bde511db2382b322b48d
Ashrith Sagar Yedlapalli <ashrith9sagar@gmail.com>
2022-12-06 07:13:22 +0530

    c0.1.63 masonry and imagesLoaded grid progress layout added

--- static/script.js
+++ static/script.js
@@ -33,3 +33,18 @@ function backToTop() {
 }
 
 // ------------------------------------------------------------------------
+
+let grid = document.querySelector('.masonry-grid');
+
+let msnry = new Masonry(grid, {
+  itemSelector: '.masonry-grid-item',
+  percentPosition: true
+});
+
+imagesLoaded(grid).on('progress', function () {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
+
+
+// ------------------------------------------------------------------------
