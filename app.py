c9036c937ff747511f137c9a7d4f67abed704541
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-07-26 21:01:00 +0530

    app.py;
    frozen flask gh-pages;

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+Freeze Flask app into GitHub Pages
+https://github.com/AshrithSagar/frozen-flask-gh-pages
+"""
+
 from os import path
 from pathlib import Path
 
@@ -7,10 +12,18 @@ from flask_frozen import Freezer
 template_folder = path.abspath("./wiki")
 
 app = Flask(__name__, template_folder=template_folder)
-# app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
-app.config["FREEZER_DESTINATION"] = "public"
+# Enter your GitHub Pages URL here instead of frozen-flask-gh-pages
+app.config["FREEZER_BASE_URL"] = "https://docs/"
+
+# As configured in GitHub Pages Settings
+app.config["FREEZER_DESTINATION"] = "docs"
+
 app.config["FREEZER_RELATIVE_URLS"] = True
 app.config["FREEZER_IGNORE_MIMETYPE_WARNINGS"] = True
+app.config["FREEZER_DESTINATION_IGNORE"] = [
+    ".nojekyll",
+    "static/assets/",
+]  # For GitHub Pages
 freezer = Freezer(app)
 
 
@@ -36,4 +49,8 @@ def pages(page):
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    freezer.freeze()  # Freeze the app into FREEZER_DESTINATION
+    # freezer.serve()  # Serve the app locally from FREEZER_DESTINATION
+
+    # freezer.run()  # Choose for URL checking
+    app.run(port=8080, debug=True)  # Choose to run locally from Flask
