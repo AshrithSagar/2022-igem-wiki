f0dcdeac8b108e324c882a66e394804066814eea
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-07-26 20:53:00 +0530

    app.py formatted;

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from pathlib import Path
 from flask import Flask, render_template
 from flask_frozen import Freezer
 
-
-template_folder = path.abspath('./wiki')
+template_folder = path.abspath("./wiki")
 
 app = Flask(__name__, template_folder=template_folder)
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
-app.config['FREEZER_DESTINATION'] = 'public'
-app.config['FREEZER_RELATIVE_URLS'] = True
-app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+# app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
+app.config["FREEZER_DESTINATION"] = "public"
+app.config["FREEZER_RELATIVE_URLS"] = True
+app.config["FREEZER_IGNORE_MIMETYPE_WARNINGS"] = True
 freezer = Freezer(app)
 
 
@@ -25,14 +24,14 @@ def serve():
     freezer.run()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('pages/index.html')
+    return render_template("pages/index.html")
 
 
-@app.route('/<page>')
+@app.route("/<page>")
 def pages(page):
-    return render_template(str(Path('pages') / (page.lower() + '.html')))
+    return render_template(str(Path("pages") / (page.lower() + ".html")))
 
 
 # Main Function, Runs at http://0.0.0.0:8080
